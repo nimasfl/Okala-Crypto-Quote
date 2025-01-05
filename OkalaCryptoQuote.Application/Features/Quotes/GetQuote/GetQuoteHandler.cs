76833b1fa2077a057eca9b71e8089fc49199051e
Nima Soufiloo <nima.soufiloo@gmail.com>
2025-01-05 12:31:48 +0330

    added validations

--- OkalaCryptoQuote.Application/Features/Quotes/GetQuote/GetQuoteHandler.cs
+++ OkalaCryptoQuote.Application/Features/Quotes/GetQuote/GetQuoteHandler.cs
@@ -1,25 +1,49 @@
 ﻿using OkalaCryptoQuote.Application.Abstractions;
+using OkalaCryptoQuote.Domain.Features.Quotes;
 
 namespace OkalaCryptoQuote.Application.Features.Quotes.GetQuote;
 
-public class GetQuoteHandler(IExchangeRatesApi exchangeRatesApi, ICoinMarketCapApi coinMarketCapApi): IGetQuoteHandler
+public class GetQuoteHandler(IExchangeRatesApi exchangeRatesApi, ICoinMarketCapApi coinMarketCapApi) : IGetQuoteHandler
 {
     public async Task<Result<GetQuoteResponse>> GetQuote(GetQuoteRequest request, CancellationToken ct)
     {
         const string baseCurrency = "USD";
-        var rates = await exchangeRatesApi.GetLatestRates(ct);
-        var cryptoInfo = await coinMarketCapApi.GetCryptoDetail(request.CryptoCode, ct);
+        var validationResult = ValidateRequest(request);
+        if (validationResult.IsSuccess == false)
+        {
+            return Result.Failure<GetQuoteResponse>(validationResult.Error);
+        }
+
+        var ratesResult = await exchangeRatesApi.GetLatestRates(ct);
+        if (ratesResult.IsSuccess == false)
+        {
+            return Result.Failure<GetQuoteResponse>(ratesResult.Error);
+        }
+
+        var cryptoInfoResult = await coinMarketCapApi.GetCryptoDetail(request.CryptoCode, ct);
+        if (cryptoInfoResult.IsSuccess == false)
+        {
+            return Result.Failure<GetQuoteResponse>(cryptoInfoResult.Error);
+        }
 
         var prices = new Dictionary<string, decimal?>();
-        foreach (var currency in rates.Value.Rates.Keys)
+        foreach (var currency in ratesResult.Value.Rates.Keys)
         {
-            var baseRatio = rates.Value.Rates[baseCurrency];
-            if (rates.Value.Rates.TryGetValue(currency, out var ratio))
+            var baseRatio = ratesResult.Value.Rates[baseCurrency];
+            if (ratesResult.Value.Rates.TryGetValue(currency, out var ratio))
             {
-                prices.Add(currency,cryptoInfo.Value.Price * ratio/baseRatio);
+                prices.Add(currency, cryptoInfoResult.Value.Price * ratio / baseRatio);
             }
         }
 
-        return new GetQuoteResponse(cryptoInfo.Value.Slug, cryptoInfo.Value.Symbol, prices);
+        return new GetQuoteResponse(cryptoInfoResult.Value.Slug, cryptoInfoResult.Value.Symbol, prices);
+    }
+
+    private static Result ValidateRequest(GetQuoteRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.CryptoCode))
+        {
+            return QuoteError.CryptoCodeIsEmpty;
+        }
     }
 }
--- OkalaCryptoQuote.Domain/Features/Quotes/QuoteError.cs
+++ OkalaCryptoQuote.Domain/Features/Quotes/QuoteError.cs
@@ -4,5 +4,5 @@ public static class QuoteError
 {
     private const string FeatureName = "Quote";
 
-    public static readonly Error Test = Error.Problem(FeatureName, "Test");
+    public static readonly Error CryptoCodeIsEmpty = Error.Invalid(FeatureName, "crypto code must have value");
 }
